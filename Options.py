29be34e6a8a3e79d0834fa53a32eb0ad070f4cd6
vedant.shah231 <vedant.shah231@gmail.com>
2023-07-11 20:26:19 -0400

    Option class to calculate IV

--- Options.py
+++ Options.py
@@ -1,11 +1,60 @@
-import jax.numpy as np
-from jax.scipy.stats import norm
-from jax import grad
+from req_import import *
+N_prime = norm.pdf
 
-class EuropeanCall:
+class Option(ABC):
+    _MAX_TRY = 1000
+    _ONE_CENT = 0.01
 
+    @validate_call
+    def __init__(
+        self, c_p: Literal['C', 'P'], asset_price, option_market_price, strike_price,
+        time_to_expiration, risk_free_rate
+            ):
+        self.c_p = c_p
+        self.asset_volatility = self.find_iv_newton(S=asset_price,
+                                                    K=strike_price,
+                                                    r=risk_free_rate,
+                                                    t=time_to_expiration,
+                                                    market_price=option_market_price)
+        inputs = np.array([asset_price, self.asset_volatility, strike_price, time_to_expiration, risk_free_rate])
+        self.option = EuropeanCall(inputs) if c_p == 'C' else EuropeanPut(inputs)
+
+    #
+    # @abstractmethod
+    # def price(
+    #         self, asset_price, asset_volatility, strike_price,
+    #         time_to_expiration, risk_free_rate
+    # ):
+    #     raise NotImplementedError
+
+    @staticmethod
+    def vega(S, K, r, t, sigma):
+        d1 = (log(S / K) + (r + sigma ** 2 / 2) * t) / (sigma * sqrt(t))
+        return S * N_prime(d1) * sqrt(t)
+
+    @staticmethod
+    def _vega_d1(S, d1, t):
+        return S * N_prime(d1) * sqrt(t)
+
+    def find_iv_newton(self, S, K, r, t, market_price):
+        sigma_guess = 0.5
+        for i in range(Option._MAX_TRY):
+            if self.c_p == 'C':
+                bs_price = EuropeanCall.call_price(S, sigma_guess, K, t, r)
+            else:
+                bs_price = EuropeanPut.put_price(S, sigma_guess, K, t, r)
+            diff = market_price - bs_price
+            if abs(diff) < Option._ONE_CENT:
+                return sigma_guess
+            d1 = (log(S / K) + (r + sigma_guess ** 2 / 2) * t) / (sigma_guess * sqrt(t))
+            vega = Option._vega_d1(S, d1, t)
+            sigma_guess += diff / vega
+        return sigma_guess
+
+class EuropeanCall():
+    @staticmethod
     def call_price(
-        self, asset_price, asset_volatility, strike_price,
+        asset_price, asset_volatility, strike_price,
         time_to_expiration, risk_free_rate
             ):
         b = np.exp(-risk_free_rate*time_to_expiration).astype('float')
@@ -23,6 +72,7 @@ class EuropeanCall:
     def __init__(
         self, inputs
             ):
+
         self.asset_price = inputs[0]
         self.asset_volatility = inputs[1]
         self.strike_price = inputs[2]
@@ -30,17 +80,16 @@ class EuropeanCall:
         self.risk_free_rate = inputs[4]
         self.price = self.call_price(self.asset_price, self.asset_volatility, self.strike_price, self.time_to_expiration, self.risk_free_rate)
 
-        self.gradient_func = grad(self.call_price, (0, 1, 3, 4))
-        self.delta, self.vega, self.theta, self.rho = self.gradient_func(inputs[0], inputs[1], inputs[2], inputs[3], inputs[4])
+        self.gradient_func = grad(self.call_price, (0, 1, 3))
+        self.delta, self.vega, self.theta = self.gradient_func(inputs[0], inputs[1], inputs[2], inputs[3], inputs[4])
         self.theta /= -365
         self.vega /= 100
-        self.rho /= 100
 
 
-class EuropeanPut:
-
+class EuropeanPut():
+    @staticmethod
     def put_price(
-        self, asset_price, asset_volatility, strike_price,
+        asset_price, asset_volatility, strike_price,
         time_to_expiration, risk_free_rate
             ):
         b = np.exp(-risk_free_rate*time_to_expiration)
@@ -70,6 +119,20 @@ class EuropeanPut:
         self.vega /= 100
 
 
-inputs = np.array([4000, 0.2, 3500, 365/365, 0.01])
-option = EuropeanCall(inputs.astype('float'))
-print(option.delta, option.vega, option.theta)
+#Usage: def __init__(
+#        self, c_p: Literal['C', 'P'], asset_price, option_market_price, strike_price,
+#        time_to_expiration, risk_free_rate
+#            ):
+
+
+call = Option(c_p='C',
+              asset_price=100,
+              option_market_price=10,
+              strike_price=105,
+              time_to_expiration=365/365,
+              risk_free_rate=0.00)
+
+#option = EuropeanCall(inputs.astype('float'))
+#print(option.find_iv_newton())
+
+print(call.option.asset_volatility, call.option.delta, call.option.vega, call.option.theta, call.option.price)
--- req_import.py
+++ req_import.py
@@ -8,3 +8,8 @@ from typing import Optional, Literal, Any, List
 from pydantic import validate_call, Field, validate_arguments
 import re
 from datetime import datetime
+import jax.numpy as np
+from jax.numpy import log, sqrt, exp, pi
+from jax.scipy.stats import norm
+from jax import grad
+from abc import ABC, abstractmethod
